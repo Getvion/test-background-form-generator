2b26e1cb029a6e8ed495e489b444b6ff830a85ac
Oleg Somov <gog0125@mail.ru>
2022-11-21 17:31:37 +0300

    add sumbit button component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,25 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputComponent } from './input/input.component';
 import { CheckboxComponent } from './checkbox/checkbox.component';
-import { DropdownComponent } from './dropdown/dropdown.component';
 import { NumberInputComponent } from './number-input/number-input.component';
 import { AddInputComponent } from './add-input/add-input.component';
+import { DropdownComponent } from './dropdown/dropdown.component';
+import { SubmitButtonComponent } from './submit-button/submit-button.component';
 
 @NgModule({
   declarations: [
     InputComponent,
     CheckboxComponent,
-    DropdownComponent,
     NumberInputComponent,
     AddInputComponent,
+    DropdownComponent,
+    SubmitButtonComponent,
   ],
   imports: [CommonModule],
   exports: [
     InputComponent,
     CheckboxComponent,
-    DropdownComponent,
     NumberInputComponent,
     AddInputComponent,
+    DropdownComponent,
+    SubmitButtonComponent,
   ],
 })
 export class SharedModule {}
