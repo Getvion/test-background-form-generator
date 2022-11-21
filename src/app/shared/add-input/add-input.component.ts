3bff59958d42f9e8fffa3a024260407656682fd1
Oleg Somov <gog0125@mail.ru>
2022-11-21 10:36:50 +0300

    add adi-input button component

--- src/app/shared/add-input/add-input.component.ts
+++ src/app/shared/add-input/add-input.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-input.component.scss'],
 })
 export class AddInputComponent implements OnInit {
-  constructor() {}
+  isActive = false;
+
+  // constructor() {}
 
   ngOnInit(): void {}
+
+  toggleVeiw() {
+    this.isActive = !this.isActive;
+  }
+
+  onDelete() {
+    this.toggleVeiw();
+  }
 }
