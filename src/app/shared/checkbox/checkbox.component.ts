7ac0659306d5a2512f51cbc14c2f9cda364a4b7b
Oleg Somov <gog0125@mail.ru>
2022-11-21 17:29:41 +0300

    add checkbox component

--- src/app/shared/checkbox/checkbox.component.ts
+++ src/app/shared/checkbox/checkbox.component.ts
@@ -1,12 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-checkbox',
   templateUrl: './checkbox.component.html',
   styleUrls: ['./checkbox.component.scss'],
 })
-export class CheckboxComponent implements OnInit {
-  constructor() {}
+export class CheckboxComponent {
+  // @Input() labelText: string = '';
 
-  ngOnInit(): void {}
+  @Input() text: string = '';
+
+  @Input() checkboxData: { text: string; isChecked: boolean }[] = [];
+
+  public isAllChecked = false;
+  // public isChecked = false;
+
+  // public regOnChange = (elem: boolean) => {};
+
+  // public regOnTouched = () => {};
+
+  // registerOnChange(fn: any): void {
+  // this.regOnChange = fn;
+  // }
+
+  // registerOnTouched(fn: any): void {
+  //   this.regOnTouched = fn;
+  // }
+
+  // writeValue(value: any) {
+  // pre-populate value
+  // if (value) {
+  // this.isChecked = value;
+  // }
+  // }
+
+  onAllCheck() {}
+
+  onChanged($event: Event) {
+    console.log($event);
+
+    // on change
+    // this.isChecked = ($event.target as HTMLInputElement).checked as boolean;
+    // this.regOnChange(($event.target as HTMLInputElement).checked);
+  }
 }
